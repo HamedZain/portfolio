66e2d15a4214f96f39f2954e6835ec1679b7f44e
HamedZain <syrgst@gmail.com>
2022-05-03 00:20:01 +0300

    Use Page component in index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from 'next'
+import { Page } from "@/components"
 
 const Home: NextPage = () => {
-	return (
-		<div>Home Page</div>
-	)
+	return <Page withHeader withFooter title="Home | H.Zain">
+		<h1>Hello World, This is my portfolio</h1>
+	</Page>
 }
 
 export default Home
