04fcbda05c43b0bc65bfd530fa5b9fc6a27b8bcc
HamedZain <syrgst@gmail.com>
2022-05-05 02:33:18 +0300

    Create All Articles page

---
+++ src/pages/article/index.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next"
+import { Page } from "@/components"
+
+const Articles: NextPage = () => {
+	return <Page title="Articles | Hamed Zain">
+		<h1>Articles List</h1>
+	</Page>
+}
+
+export default Articles
