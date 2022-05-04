07718f86e08817eb3e2cd1a5e04dc2df96d5519c
HamedZain <syrgst@gmail.com>
2022-05-05 02:31:17 +0300

    Add meta tags

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -7,11 +7,29 @@ interface PageProps {
 	title: string
 	withHeader?: boolean
 	withFooter?: boolean
+
+	author?: string
+	description?: string
+	keywords?: string[]
 }
 
-export const Page: FC<PropsWithChildren<PageProps>> = props => {
+export const Page: FC<PropsWithChildren<PageProps>> = ({
+	author = 'Hamed Zain',
+	keywords = [],
+
+	...props
+}) => {
 	return <>
 		<Head>
+			<meta charSet="UTF-8" />
+			<meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+			<meta name="viewport"
+			      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+
+			<meta name="author" content={author} />
+			<meta name="description" content={props.description} />
+			<meta name="keywords" content={keywords.join(', ')} />
+
 			<title>{props.title}</title>
 		</Head>
 
@@ -19,7 +37,7 @@ export const Page: FC<PropsWithChildren<PageProps>> = props => {
 
 			{props.withHeader && <Header />}
 
-			<div className={styles.content}>{props.children}</div>
+			<main className={styles.content}>{props.children}</main>
 
 			{props.withFooter && <Footer />}
 
