1b2eac7774ff11798ff4a2f9640f35373d014ca2
HamedZain <syrgst@gmail.com>
2022-05-05 02:19:41 +0300

    Show articles list in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,32 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps, NextPage } from 'next'
 import { Page } from "@/components"
+import { getAllArticles } from '@/api'
+import { Article } from "../types/Article"
+import {default as Link} from 'next/link'
 
-const Home: NextPage = () => {
+const Home: NextPage<StaticProps> = props => {
+
 	return <Page withHeader withFooter title="Home | H.Zain">
 		<h1>Hello World, This is my portfolio</h1>
+
+		<h2>Articles</h2>
+		{props.articles.map(article => <Link key={article.id} href={`article/${article.slug}`}>{article.name}</Link>)}
 	</Page>
+}
+
+type StaticProps = {
+	articles: Article[]
+}
+
+export const getStaticProps: GetStaticProps<StaticProps> = async () => {
+
+	const articles = await getAllArticles()
+
+	return {
+		props: {
+			articles,
+		},
+	}
 }
 
 export default Home
