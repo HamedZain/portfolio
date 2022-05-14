2ab0e31e8d32c1c432a8f88bf8f31accbb730590
HamedZain <syrgst@gmail.com>
2022-05-14 10:48:34 +0300

    Update Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,19 @@
 import { FC } from "react"
+import styles from './Header.module.scss'
+import { Button, Container } from '@/components'
+import { DribbbleIcon, GithubIcon, SunIcon } from '@/icons'
 
 export const Header: FC = () => {
 	return (
 		<header>
-			<h1>Header</h1>
+			<Container className={styles.container}>
+				<h1 className={styles.brand}>{`>_h.zain`}</h1>
+				<nav>
+					<Button icon><GithubIcon /></Button>
+					<Button icon><DribbbleIcon /></Button>
+					<Button icon><SunIcon /></Button>
+				</nav>
+			</Container>
 		</header>
 	)
 }
