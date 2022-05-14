209b2b2480e55376bc18fdc40e4fd867e5035656
HamedZain <syrgst@gmail.com>
2022-05-14 10:47:16 +0300

    Add and export svg icons

---
+++ src/icons/index.ts
@@ -0,0 +1,9 @@
+// Arrows
+export { ReactComponent as ArrowRightIcon } from './svg/arrow-right.svg'
+
+// Others
+export { ReactComponent as SunIcon } from './svg/sun.svg'
+export { ReactComponent as MoonIcon } from './svg/moon.svg'
+export { ReactComponent as GithubIcon } from './svg/github.svg'
+export { ReactComponent as DribbbleIcon } from './svg/dribbble.svg'
+
