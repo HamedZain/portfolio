5f0396fd0a78dd4e5a765f078a7a4b1f89d45383
HamedZain <syrgst@gmail.com>
2022-05-14 11:09:02 +0300

    Add aria-label to button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,18 +3,19 @@ import styles from "./Button.module.scss"
 import {className as cn} from "@/className"
 
 interface ButtonProps {
+	ariaLabel?: string
 	icon?: boolean
 	iconButton?: boolean
 	onClick?: () => void
 }
 
-export const Button: FC<PropsWithChildren<ButtonProps>> = props => {
+export const Button: FC<PropsWithChildren<ButtonProps>> = ({ ariaLabel, ...props }) => {
 
 	const className = cn(styles.el, {
 		[styles.icon]: Boolean(props.icon)
 	})
 
-	return <button className={className} {...props}>
+	return <button className={className} aria-label={ariaLabel} {...props}>
 		{props.children}
 	</button>
 }
