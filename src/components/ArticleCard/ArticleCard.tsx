9e1f4be2175bc3bec7fb2fb2bcd3e375c74c1391
HamedZain <syrgst@gmail.com>
2022-05-14 10:49:15 +0300

    Update article component

--- src/components/ArticleCard/ArticleCard.tsx
+++ src/components/ArticleCard/ArticleCard.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react"
+import styles from "./ArticleCard.module.scss"
 import { Article } from "../../types/Article"
 
 interface ArticleCardProps {
@@ -6,5 +7,19 @@ interface ArticleCardProps {
 }
 
 export const ArticleCard: FC<ArticleCardProps> = ({ article }) => {
-	return <div>{article.name}</div>
+	return <div className={styles.el}>
+		<div className={styles.langIcon}>
+			<img src="lang/javascript.svg" alt="javascript" />
+		</div>
+		<div className={styles.wrapper}>
+			<h3>{article.name}</h3>
+			<p>Read the latest guidance to learn how dynamic color works, learn how to apply it to your brand, or
+			   generate your own theme</p>
+			<div className={styles.tags}>
+				<div className={styles.tag}>Functional Programming</div>
+				<div className={styles.tag}>Closure</div>
+				<div className={styles.tag}>Javascript</div>
+			</div>
+		</div>
+	</div>
 }
