bd45dbb328a7a32738207e2d2090a1a52afa3464
HamedZain <syrgst@gmail.com>
2022-05-14 12:12:28 +0300

    Wrap Article card with anchor element

--- src/components/ArticleCard/ArticleCard.tsx
+++ src/components/ArticleCard/ArticleCard.tsx
@@ -1,26 +1,32 @@
 import { FC } from "react"
 import styles from "./ArticleCard.module.scss"
 import { Article } from "../../types/Article"
-import {default as Image} from 'next/image'
+import { default as Image } from 'next/image'
+import { default as Link } from 'next/link'
 
 interface ArticleCardProps {
 	article: Article
 }
 
 export const ArticleCard: FC<ArticleCardProps> = ({ article }) => {
-	return <div className={styles.el}>
-		<div className={styles.langIcon}>
-			<Image src="/lang/javascript.svg" alt="javascript" width={32} height={32} />
-		</div>
-		<div className={styles.wrapper}>
-			<h3>{article.name}</h3>
-			<p>Read the latest guidance to learn how dynamic color works, learn how to apply it to your brand, or
-			   generate your own theme</p>
-			<div className={styles.tags}>
-				<div className={styles.tag}>Functional Programming</div>
-				<div className={styles.tag}>Closure</div>
-				<div className={styles.tag}>Javascript</div>
+
+	const link = `/article/${article.slug}`
+
+	return <Link href={link}>
+		<div className={styles.el}>
+			<div className={styles.langIcon}>
+				<Image src="/lang/javascript.svg" alt="javascript" width={32} height={32} />
+			</div>
+			<div className={styles.wrapper}>
+				<h3>{article.name}</h3>
+				<p>Read the latest guidance to learn how dynamic color works, learn how to apply it to your brand, or
+				   generate your own theme</p>
+				<div className={styles.tags}>
+					<div className={styles.tag}>Functional Programming</div>
+					<div className={styles.tag}>Closure</div>
+					<div className={styles.tag}>Javascript</div>
+				</div>
 			</div>
 		</div>
-	</div>
+	</Link>
 }
